2ae467dd78eb03a7898ff8b36a20dcf67a61f3c1
AzizIsa <azizisa.us@gmail.com>
2018-12-11 01:49:16 -0500

    4th draft

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,6 +2,8 @@
 import os
 import csv
 
+
+
 #Create a path for the file
 csvpath = os.path.join('Resources', 'election_data.csv')
 
@@ -11,5 +13,27 @@ with open(csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ',')
 
     csv_header = next(csvreader)
+    csv_reader = list(csvreader)
+    rowcount = int(len(csv_reader))
+    print(rowcount)
+
+    uniq_list=[]
+    for row in csv_reader:
+        if row[2] not in uniq_list:
+            uniq_list.append(row[2])
+        else:
+            continue
+    print(uniq_list)
+
+
+
+
+
+
+
+
+
+
+
 
 

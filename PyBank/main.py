14c83ff22c9b6fb3bfeda677527e0a636ff1ce2b
Aziz Isamedinov <44539845+AzizIsa@users.noreply.github.com>
2018-12-12 01:09:24 -0500

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -42,7 +42,7 @@ with open(csvpath, newline='') as csvfile:
     greatestInc= max(diff)
     greatestDec = min(diff)
 
-    #Find
+    #Find months of min/max values
     max_index = diff.index(greatestInc)
     max_month = сsv_reader[max_index+1][0]
 
